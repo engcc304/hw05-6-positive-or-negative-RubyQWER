9b48ddfaf9ea3386fe323973f0e1564a39369958
RubyQWER <Chanatpong1234@gmail.com>
2023-08-17 01:44:18 +0700

    Hw05-6

--- Homework/HW05/hw05-6-pos-or-neg.cpp
+++ Homework/HW05/hw05-6-pos-or-neg.cpp
@@ -16,3 +16,21 @@
         Negative
 
 */
+#include <stdio.h>
+
+int main() {
+
+    int num ;
+
+    printf("Input : ") ;
+
+    scanf("%d",&num) ;
+
+    if (num >= 0) {
+
+        printf("Positive") ;
+
+    }   else printf("Negative") ;
+    
+    return 0 ;
+}//end function
